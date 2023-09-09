484d020683880b51124a1245aaccb65ccb455ed7
FedericoRodriguez7 <tafedericoemanuel@gmail.com>
2023-09-09 16:11:31 -0300

    finish

--- api/index.js
+++ api/index.js
@@ -9,6 +9,7 @@ const multer = require("multer");
 const fs = require("fs")
 const User = require("./models/User.js")
 const Place = require("./models/Place.js")
+const Booking = require ("./models/Booking.js")
 require("dotenv").config()
 const app = express();
 
@@ -26,10 +27,23 @@ app.use(cors({
 
 mongoose.connect(process.env.MONGO_URL)
 
+function getUserDataFromReq(req){
+    return new Promise((resolve, reject) => {
+        jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+        if (err) throw err;
+        resolve (userData);
+    }) 
+    })
+
+   
+}
+
 app.get("/test", (req, res) => {
     res.json("test ok");
 
 });
+
+
 
 app.post("/register", async (req, res) => {
     const { name, email, password } = req.body;
@@ -127,13 +141,13 @@ res.json(uploadedFiles);
 app.post("/places",(req, res) =>{
     const {token} = req.cookies;
     const {title, address, addedPhotos, description,
-         perks, extraInfo, checkIn, checkOut, maxGuests} = req.body 
+         perks, extraInfo, checkIn, checkOut, maxGuests, price} = req.body 
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
         if (err) throw err;
        const placeDoc = await Place.create({
     owner: userData.id,
-    title, address, addedPhotos, description,
-         perks, extraInfo, checkIn, checkOut, maxGuests 
+    title, address, photos:addedPhotos, description,
+         perks, extraInfo, checkIn, checkOut, maxGuests, price 
 })
       res.json(placeDoc); 
 
@@ -141,5 +155,68 @@ app.post("/places",(req, res) =>{
 
 
 })
+
+app.get("/user-places", (req,res) =>{
+    const {token} = req.cookies;
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+        const {id} = userData;
+        res.json( await Place.find({owner:id}) )
+    })
+})
+
+app.get("/places/:id", async (req, res) =>{
+    const {id} = req.params;
+    res.json(await Place.findById(id))
+})
+
+app.put("/places", async (req,res) =>{
+    const {token} = req.cookies;
+    const {id, title, address, addedPhotos, description,
+         perks, extraInfo, checkIn, checkOut, maxGuests, price} = req.body 
+         
+         jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+            const placeDoc = await Place.findById(id);
+if (userData.id === placeDoc.owner.toString()) {
+    placeDoc.set({
+        owner: userData.id,
+        title, address, photos:addedPhotos, description,
+             perks, extraInfo, checkIn, checkOut, maxGuests,price 
+    }
+
+    )
+    await placeDoc.save();
+    res.json("ok")
+}
+         })
+})
+
+app.get("/places", async (req,res) =>{
+    res.json( await Place.find())
+})
+
+app.post("/bookings", async (req,res) =>{
+    const userData = await getUserDataFromReq(req);
+    const {place,checkIn,checkOut,numberOfGuests,name,phone,price
+    } = req.body;
+    Booking.create({
+        place,checkIn,checkOut,numberOfGuests,name,phone,price,
+        user:userData.id,
+    }).then((doc) =>{
+        res.json(doc);
+    }).catch((err) => {
+        throw err;
+    })
+})
+
+
+
+
+
+app.get("/bookings", async (req,res) =>{
+   const userData = await getUserDataFromReq(req);
+   res.json(await Booking.find({user:userData.id}).populate("place"));
+
+});
+
 
 app.listen(4000);
--- my-react-app/src/pages/PlacesFormPage.jsx
+++ my-react-app/src/pages/PlacesFormPage.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Perks from "../Perks";
 import PhotosUploader from "./PhotosUploader";
 import axios from "axios";
 import AccountNav from "./AccountNav";
-import { Navigate } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 
 
 
 
 export default function PlacesFormPage() {
-    
+    const { id } = useParams();
     const [title, setTitle] = useState("");
     const [address, setAddress] = useState("");
     const [addedPhotos, setAddedPhotos] = useState("");
@@ -20,85 +20,122 @@ export default function PlacesFormPage() {
     const [checkOut, setCheckOut] = useState("");
     const [maxGuests, setMaxGuests] = useState(1);
     const [redirect, setRedirect] = useState(false);
+    const [price, setPrice] = useState(100);
+    useEffect(() => {
+        if (!id) {
+            return;
+        }
+        axios.get("/places/" + id).then(response => {
+            const { data } = response;
+            setTitle(data.title);
+            setAddress(data.address);
+            setAddedPhotos(data.photos);
+            setDescription(data.description);
+            setPerks(data.perks);
+            setExtraInfo(data.extraInfo);
+            setCheckIn(data.checkIn);
+            setCheckOut(data.checkOut);
+            setMaxGuests(data.maxGuests);
+            setPrice(data.price);
+        })
+    }, [id])
 
-    function inputHeader(text){
+    function inputHeader(text) {
         return (
             <h2 className="text-2xl mt-4">{text}</h2>
         )
     }
 
-    function inputDescription(text){
+    function inputDescription(text) {
         return (
             <p className="text-gray-500 text-sm">{text}</p>
         )
     }
 
-    function preInput(header,description){
+    function preInput(header, description) {
         return (
-           <>
-           {inputHeader(header)}
-           {inputDescription(description)}
-           </>
+            <>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </>
         )
-        
+
     }
 
-     async function addNewPlace(ev){
+    async function savePlace(ev) {
         ev.preventDefault();
-      await axios.post("/places", {title, address, addedPhotos, description,
-        perks, extraInfo, checkIn,
-         checkOut, maxGuests});
-                setRedirect(true)
-            }
+        const placeData = {
+            title, address, addedPhotos, description,
+            perks, extraInfo, checkIn,
+            checkOut, maxGuests, price
+        }
+        if (id) {
+            //update
+            await axios.put("/places", {
+                id,
+                ...placeData
+            });
+            setRedirect(true)
+        } else {
+            //new place
+            await axios.post("/places", placeData
+            );
+            setRedirect(true)
+        }
+    }
 
 
-            if (redirect) {
-                return <Navigate to={"/account/places"} />
-            }
-            
-return(
-    <div>
-        <AccountNav />
-                    <form onSubmit={addNewPlace}>
-                        {preInput("Title","title for your place, should be short and catchy as in advertisement")}
-                        <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My lovely appartament" />
-                        {preInput("Address","Address to this place")}
-                        <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="address" />
-                        {preInput('Photos','more = better')}
-                        <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos}/>
 
-                        {preInput('Description','description of the place')}
-                        <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
-                        {preInput('Perks','select all the perks of your place')}
-                        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-col-4 lg:grid-cols-6">
-                        <Perks selected={perks} onChange={setPerks}/>
-                        </div>
+    if (redirect) {
+        return <Navigate to={"/account/places"} />
+    }
 
-                        {preInput('Extra info','house rules, etc')}
-                        <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
-                        {preInput('Check in&out times','add check in and out times, remember to have some time window for cleaning the room between guests')}
-                        <div  className="grid gap-2 sm:grid-cols-3">
-                            <div>
-                                <h3 className="mt-2 -mb-1">Check in time</h3>
-                                <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)} placeholder="14" />
-                            </div>
-                            <div>
-                            <h3 className="mt-2 -mb-1">Check out time</h3>
-                                <input type="text" value={checkOut} onChange={ev => setCheckOut(ev.target.value)} placeholder="11" />
-                            </div>
-                            <div>
-                            <h3 className="mt-2 -mb-1">Max number of guests</h3>
-                                <input value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)} type="number" />
-                            </div>
+    return (
+        <div>
+            <AccountNav />
+            <form onSubmit={savePlace}>
+                {preInput("Title", "title for your place, should be short and catchy as in advertisement")}
+                <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My lovely appartament" />
+                {preInput("Address", "Address to this place")}
+                <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="address" />
+                {preInput('Photos', 'more = better')}
+                <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
 
-
-                        </div>
-                        
-                            <button className="primary my-4">Save</button>
-                        
-                    </form>
+                {preInput('Description', 'description of the place')}
+                <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
+                {preInput('Perks', 'select all the perks of your place')}
+                <div className="grid mt-2 gap-2 grid-cols-2 md:grid-col-4 lg:grid-cols-6">
+                    <Perks selected={perks} onChange={setPerks} />
                 </div>
-)
+
+                {preInput('Extra info', 'house rules, etc')}
+                <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
+                {preInput('Check in&out times', 'add check in and out times, remember to have some time window for cleaning the room between guests')}
+                <div className="grid gap-2 grid-cols-2 md:grid-cols-4">
+                    <div>
+                        <h3 className="mt-2 -mb-1">Check in time</h3>
+                        <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)} placeholder="14" />
+                    </div>
+                    <div>
+                        <h3 className="mt-2 -mb-1">Check out time</h3>
+                        <input type="text" value={checkOut} onChange={ev => setCheckOut(ev.target.value)} placeholder="11" />
+                    </div>
+                    <div>
+                        <h3 className="mt-2 -mb-1">Max number of guests</h3>
+                        <input value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)} type="number" />
+                    </div>
+                    <div>
+                        <h3 className="mt-2 -mb-1">Price per night</h3>
+                        <input value={price} onChange={ev => setPrice(ev.target.value)} type="number" />
+                    </div>
+
+                </div>
+
+                <button className="primary my-4">Save</button>
+
+            </form>
+        </div>
+    )
 }
 
 
